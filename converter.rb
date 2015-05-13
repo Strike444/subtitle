8c5192124b032dab8f3ae21a2e33e5f57117bd7b
Andrey Shimanovich <strikebgu84@mail.ru>
2015-05-13 20:49:43 +0300

    Уже что-то сделал (Так нельзя писать коммиты) Вдруг их кто-то будет читать кроме автора.

--- converter.rb
+++ converter.rb
@@ -1,6 +1,22 @@
-string = File.open("{GoldFishSubs} Padam Padam ep. 17.ass", 'r'){|file| file.read}
-#puts string
-puts string.scan(/Dialogue.*/)
+class Converter
+#  def initialize(p)
+
+  string = File.open("{GoldFishSubs} Padam Padam ep. 17.ass", 'r'){|file| file.read}
+  bez_zag = string.scan(/Dialogue.*/)
+  a = bez_zag.join("~@#")
+  while a.include?("Dialogue:") do
+       a.slice!(/Dialogue:../)
+     end
+  a.gsub!(/~@#,/, "~@#0")
+  a.gsub!(/^./,'0')
+  a.gsub!(/(?<first>\d{2}):(?<second>\d{2}):(?<third>\d{2})\.(?<fourth>\d{2}),(?<fifth>\d):(?<sixth>\d{2}):(?<seventh>\d{2})\.(?<eighth>\d{2}),/,'\k<first>:\k<second>:\k<third>,0\k<fourth>,,0\k<fifth>:\k<sixth>:\k<seventh>,0\k<eighth>,,/')
+  #a.gsub!(/,,.*,,\d{4},\d{4},\d{4},,/,",,")
+  puts a
+end
+
+
+
+
 #arrey = stringbezzaglaviya.split(",,")
 #puts arrey
 #arrey.drop_while {|a| a.include?("Dialogue")}
